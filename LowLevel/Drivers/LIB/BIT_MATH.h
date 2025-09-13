f88ae303a216d22deedb9d5d469e32d1c70cd031
sh-toqa <toqashalash8@gmail.com>
2025-09-13 18:58:34 +0300

    Add LIB files

---
+++ LowLevel/Drivers/LIB/BIT_MATH.h
@@ -0,0 +1,11 @@
+
+#ifndef LIB_BIT_MATH_H_
+#define LIB_BIT_MATH_H_
+
+#define SET_BIT(reg, bitN)		(reg|= (1<<bitN))
+#define CLR_BIT(reg, bitN)		(reg&= ~(1<<bitN))
+#define TOG_BIT(reg, bitN)		(reg^= (1<<bitN))
+#define GET_BIT(reg, bitN)		((reg>>bitN)&1)
+
+
+#endif /* LIB_BIT_MATH_H_ */
