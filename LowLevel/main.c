242dc2731987073c1e21a546b8e72e6128914779
eng-aly <es-aly.ahmed2027@alexu.edu.eg>
2025-09-13 20:11:31 +0300

    finish UART

--- LowLevel/main.c
+++ LowLevel/main.c
@@ -7,6 +7,28 @@ void PrintDistance(u16 distance) {
     MUSART_vSendString(" cm\r\n");
 }
 
+void int_to_str(s16 num, s8 *str) {
+    u8 i = 0, j;
+    s8 buf[10];
+
+    if (num == 0) {
+        str[0] = '0';
+        str[1] = '\0';
+        return;
+    }
+
+    while (num > 0) {
+        buf[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    // reverse into str
+    for (j = 0; j < i; j++) {
+        str[j] = buf[i - j - 1];
+    }
+    str[j] = '\0';
+}
+
 void SETUP(){
 	MRCC_vInit();
 	MRCC_vEnableClk(RCC_AHB1,RCC_GPIOA);
@@ -19,10 +41,12 @@ void SETUP(){
 	MGPIO_vPinInit(& LED4);
 	MGPIO_vPinInit(& LED5);
 	HCSR04_vInit(&us1);
+    MCAL_UART_Init(DIFF_UART,&DIFF_UART_TX_Cfg);
 }
 
 
 void LOOP(){
+		u8 buffer[10];
 		HCSR04_vTrigger(&us1);
 		MTIM_vIC_EnableInterrupt(TIM_ID_2, TIM_CHANNEL1);
 		MTIM_vIC_EnableCapture(TIM_ID_2, TIM_CHANNEL1);
@@ -41,7 +65,8 @@ void LOOP(){
 			MGPIO_vSetPinValue(GPIO_PORTA, GPIO_PIN3, GPIO_HIGH);
 		}
 		x = TIM2->CNT;
-
+		int_to_str(distance, (s8 *)buffer);
+		MCAL_UART_SendString(DIFF_UART, (u8 *)buffer);
 		DELAY_MS(100);
 }
 
